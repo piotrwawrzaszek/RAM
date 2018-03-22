989da6d0cd3d15018e910a40e5c64eae5a046f28
DESKTOP-R9U905A\piotr <piotr.wawrzaszek.1e@gmail.com>
2018-03-22 23:45:56 +0100

    Project files structure change.

---
+++ RAM.Infrastructure/Data/MenuItemProvider.cs
@@ -0,0 +1,38 @@
+﻿sing System.Collections.Generic;
+using System.Collections.ObjectModel;
+using RAM.Infrastructure.Resources;
+using RAM.Infrastructure.Wrapper;
+
+namespace RAM.Infrastructure.Data
+{
+    public class MenuItemProvider : IMenuItemProvider
+    {
+        private readonly List<MenuItem> _menuItems;
+
+        public MenuItemProvider()
+        {
+            _menuItems = new List<MenuItem>
+            {
+                new MenuItem(null) {Header = Resource.FileEN},
+                new MenuItem(null) {Header = Resource.ViewEN},
+                new MenuItem(null) {Header = Resource.EditEN},
+                new MenuItem(null) {Header = Resource.HelpEN}
+            };
+
+            _menuItems[0].ChildMenuItems =
+                new ObservableCollection<MenuItem>
+                {
+                    new MenuItem(_menuItems[0]) {Header = "Option 1"},
+                    new MenuItem(_menuItems[0]) {Header = "Option 2"},
+                    new MenuItem(_menuItems[0]) {Header = "Option 3"},
+                    new MenuItem(_menuItems[0]) {Header = "Option 4"},
+                    new MenuItem(_menuItems[0]) {Header = "Option 5"}
+                };
+        }
+
+        public IEnumerable<MenuItem> GetAllMenuItems()
+        {
+            return _menuItems;
+        }
+    }
+}
